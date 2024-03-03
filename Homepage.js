1cdcd284f5b07adbd9846afe35504dcbb03bb947
Nithyashree <Anhsirk6khaos@protonmail.com>
2024-03-02 22:38:30 -0500

    gif and changes to homepage

--- Homepage.js
+++ Homepage.js
@@ -1,52 +1,56 @@
+import PropTypes from "prop-types";
 import React from "react";
 import "./style.css";
 
-export const LandingPage = () => {
+export const LandingPage = ({ showEmpowher = true }) => {
   return (
     <div className="landing-page">
-      <div className="overlap-wrapper">
-        <div className="overlap">
-          <div className="frame">
-            <img className="group" alt="Group" src="group-997.png" />
-          </div>
-          <div className="group-wrapper">
-            <div className="overlap-group-wrapper">
+      <div className="overlap">
+        <div className="background-frame">
+          <img className="background-group" alt="Background group" src="background-group.png" />
+        </div>
+        <div className="exclipse-group">
+          <div className="eclipse-women-avatar">
+            <div className="eclipse-assets">
               <div className="overlap-group">
-                <div className="ellipse" />
+                <img className="ellipse" alt="Ellipse" src="ellipse-3.png" />
                 <img className="img" alt="Ellipse" src="ellipse-4.svg" />
               </div>
             </div>
           </div>
-          <div className="div">
-            <div className="navbar-wrapper">
-              <div className="navbar">
-                <div className="text-wrapper">Home</div>
-                <div className="text-wrapper-2">Find Doctors</div>
-                <div className="text-wrapper-2">Hospitals</div>
-                <div className="text-wrapper-2">Surgeries</div>
-              </div>
-            </div>
-            <img className="group-2" alt="Group" src="group-896.png" />
-            <div className="frame-wrapper">
-              <div className="div-wrapper">
-                <div className="text-wrapper-3">Contact Us</div>
-              </div>
-            </div>
-          </div>
-          <div className="group-3">
-            <div className="frame-2">
-              <div className="frame-3">
-                <div className="text-wrapper-4">EmpowHER</div>
-                <p className="empower-protect">
-                  &#34;Empower. Protect. Shine. Our app is every woman&#39;s shield in the digital world.&#34;
-                </p>
-              </div>
-            </div>
-          </div>
-          <img className="svg" alt="Svg" src="svg-h2d-32016369.svg" />
-          <img className="mask-group" alt="Mask group" src="mask-group.png" />
         </div>
+        <div className="contac-group">
+          <div className="nav-group">
+            <div className="nav">
+              <div className="text-wrapper">Home</div>
+              <div className="div">Social</div>
+              <div className="div">Education</div>
+              <div className="div">Personal</div>
+            </div>
+          </div>
+          <div className="contact">
+            <div className="contact-frame">
+              <div className="text-wrapper-2">Contact Us</div>
+            </div>
+          </div>
+        </div>
+        <div className="quote-group">
+          <div className="quote-frame">
+            <div className="quote">
+              <div className="text-wrapper-3">EmpowHER</div>
+              <p className="empower-protect">
+                &#34;Empower. Protect. Shine. Our app is every woman&#39;s shield in the digital world.&#34;
+              </p>
+            </div>
+          </div>
+        </div>
+        <img className="nav-image" alt="Nav image" src="nav-image.svg" />
+        <img className="mask-group" alt="Mask group" src="mask-group.png" />
       </div>
     </div>
   );
+};
+
+LandingPage.propTypes = {
+  showEmpowher: PropTypes.bool,
 };
